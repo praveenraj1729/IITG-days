8aa135c4a96c468f6ecdd42503668dec4e998176
praveenraj1729 <praveenraj2419iit@gmail.com>
2022-08-14 20:12:17 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+app.get('/',(req, res) => {
+  res.send("the server is up and running!!");
+});
+
+const CONNECTION_URL = 'mongodb+srv://praveen:praveen1234@cluster0.oikzgoy.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
